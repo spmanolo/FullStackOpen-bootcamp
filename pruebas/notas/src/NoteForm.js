3094be9cc3764f6567c474f5532ef5e89158ba6e
spmanolo <manolosan2001@gmail.com>
2023-09-01 18:50:54 +0200

    removed notes from this rep

--- pruebas/notas/src/NoteForm.js
+++
@@ -1,8 +0,0 @@
-export function NoteForm({ newNote, onNewNoteChange, handleSubmit }) {
-  return (
-    <form onSubmit={handleSubmit}>
-      <input type='text' onChange={(e) => onNewNoteChange(e.target.value)} value={newNote} />
-      <button>Crear nota</button>
-    </form>
-  )
-}
